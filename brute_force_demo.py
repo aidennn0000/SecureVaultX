ccbe54b19550864ea55ff7f6af39ed1c51ce5e68
kali <kali@kali.kali>
2026-07-13 13:05:36 -0400

    Add brute force demonstration script

---
+++ brute_force_demo.py
@@ -0,0 +1,11 @@
+passwords = [
+    "password",
+    "123456",
+    "admin123",
+    "qwerty",
+    "Password123!"
+]
+
+for pwd in passwords:
+    print(f"Trying password: {pwd}")
+    print("Login Failed")
